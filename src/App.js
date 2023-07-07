4147ca3e0aa53f41be63e3ad917580086267c365
Arthur Suassuna <89258914+ArthurSMA@users.noreply.github.com>
2023-07-07 09:40:14 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./layout/styles.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../layout/styles.css";
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <p> THIS IS A HOME PAGE</p>
+    </div>
+  );
+}
